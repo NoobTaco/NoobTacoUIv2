d527b0b37983254e4db4e3b7498afca5ff1d21fc
Mike Norton <mike@corcrastudio.com>
2022-08-11 12:02:05 -0700

    Auras

--- modules/Auras.lua
+++ modules/Auras.lua
@@ -2,18 +2,18 @@ local NoobTacoUIv2, E, L, V, P, G = unpack(select(2, ...))
 
 function NoobTacoUIv2:SetupAuras()
     -- AURAS -------------------------------------------------------------------
-    E.db["auras"]["font"] = "Montserrat-Bold"
+    E.db["auras"]["buffs"]["countFont"] = "Exo2-Medium"
+    E.db["auras"]["buffs"]["countFontSize"] = 14
+    E.db["auras"]["buffs"]["durationFontSize"] = 14
+    E.db["auras"]["buffs"]["timeFont"] = "Exo2-Medium"
+    E.db["auras"]["buffs"]["timeFontOutline"] = "NONE"
+    E.db["auras"]["buffs"]["timeFontSize"] = 12
     E.db["auras"]["cooldown"]["expiringColor"]["b"] = 0.41176470588235
     E.db["auras"]["cooldown"]["expiringColor"]["g"] = 0.38039215686275
     E.db["auras"]["cooldown"]["expiringColor"]["r"] = 0.74901960784314
-    E.db["auras"]["buffs"]["countFont"] = "Montserrat-Bold"
-    E.db["auras"]["buffs"]["timeFont"] = "Montserrat-Bold"
-    E.db["auras"]["buffs"]["timeFontOutline"] = "NONE"
-    E.db["auras"]["buffs"]["timeFontSize"] = 12
-    E.db["auras"]["buffs"]["countFontSize"] = 14
-    E.db["auras"]["buffs"]["durationFontSize"] = 14
-    E.db["auras"]["debuffs"]["countFont"] = "Montserrat-Bold"
+    E.db["auras"]["debuffs"]["countFont"] = "Exo2-Medium"
     E.db["auras"]["debuffs"]["countFontSize"] = 14
-    E.db["auras"]["debuffs"]["timeFont"] = "Montserrat-Bold"
+    E.db["auras"]["debuffs"]["timeFont"] = "Exo2-Medium"
     E.db["auras"]["debuffs"]["timeFontOutline"] = "NONE"
+    E.db["auras"]["font"] = "Exo2-Medium"
 end
