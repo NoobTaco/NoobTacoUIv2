e254b2c2ecc367ada6969f806907789891b4ec07
Mike Norton <mike@corcrastudio.com>
2022-08-11 12:23:12 -0700

    Tooltip

--- modules/Tooltips.lua
+++ modules/Tooltips.lua
@@ -2,13 +2,13 @@ local NoobTacoUIv2, E, L, V, P, G = unpack(select(2, ...))
 
 function NoobTacoUIv2:SetupTooltips()
     -- TOOLTIPS ---------------------------------------------------------------
-    E.db["tooltip"]["textFontSize"] = 14
-    E.db["tooltip"]["font"] = "Montserrat-Bold"
-    E.db["tooltip"]["headerFont"] = "Montserrat-Bold"
-    E.db["tooltip"]["healthBar"]["height"] = 10
-    E.db["tooltip"]["healthBar"]["font"] = "Montserrat-Bold"
-    E.db["tooltip"]["healthBar"]["fontOutline"] = "NONE"
-    E.db["tooltip"]["smallTextFontSize"] = 14
-    E.db["tooltip"]["colorAlpha"] = 1
+    E.db["tooltip"]["font"] = "Exo2-SemiBold"
+    E.db["tooltip"]["headerFont"] = "Exo2-Bold"
     E.db["tooltip"]["headerFontSize"] = 14
+    E.db["tooltip"]["healthBar"]["font"] = "Exo2-SemiBold"
+    E.db["tooltip"]["healthBar"]["fontOutline"] = "NONE"
+    E.db["tooltip"]["healthBar"]["height"] = 13
+    E.db["tooltip"]["itemQuality"] = true
+    E.db["tooltip"]["smallTextFontSize"] = 14
+    E.db["tooltip"]["textFontSize"] = 14
 end
